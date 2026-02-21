ddb7a80f01bc6ae8830499f2eaa24aa96fd995f5
Luc RALIJAONA <99108847+Lmar95@users.noreply.github.com>
2026-02-21 02:26:07 -0500

    Add solution for exercise-4.py

---
+++ exercise-4.py
@@ -0,0 +1,10 @@
+# Exercise 4 — Program that calculates and displays the power of X by Y
+# ─────────────────────────────────────────────────────────────────────────────
+
+def main():
+    x = float(input("Enter the base      (X): "))
+    y = int(input("Enter the exponent  (Y): "))
+    print(f"\n{x} ^ {y} = {x ** y}")
+
+if __name__ == "__main__":
+    main()
