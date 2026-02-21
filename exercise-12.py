7bfc821b234c7877b95a6a659a6fd2fd33f4be42
Luc RALIJAONA <99108847+Lmar95@users.noreply.github.com>
2026-02-21 02:26:34 -0500

    Add solution for exercise-12.py

---
+++ exercise-12.py
@@ -0,0 +1,16 @@
+# Exercise 12 — Program that returns whether two numbers have the same sign
+# ─────────────────────────────────────────────────────────────────────────────
+
+def same_sign(a: float, b: float) -> bool:
+    return (a >= 0 and b >= 0) or (a < 0 and b < 0)
+
+def main():
+    a = float(input("First  number: "))
+    b = float(input("Second number: "))
+    if same_sign(a, b):
+        print(f"\n{a} and {b} have the SAME sign.")
+    else:
+        print(f"\n{a} and {b} have DIFFERENT signs.")
+
+if __name__ == "__main__":
+    main()
